4a959e4a820939d3ed47cf4e0a7df9f459bd2d7d
WilliamJones62 <williamjones120862@gmail.com>
2018-02-14 14:35:55 -0500

    Set up Rot route, code to check if API request needed added to controller

--- app/controllers/texts_report_controller.rb
+++ app/controllers/texts_report_controller.rb
@@ -1,5 +1,13 @@
 class TextsReportController < ApplicationController
   def dashboard
-    https://api.zingle.me/v1/services/31444380-7c5f-11e7-a433-0f09c13b1321/messages?communication_direction=inbound&page=2&page_size=1000
+    @time1 = Time.new
+    @texts = Text.all
+    @contacts = Contact.all
+    @text_header = Text.first
+    @time2 = @text_header.updated_at
+    if @time1.yday != @time2.yday
+      # update the Text and Contacts tables with any new data
+      # https://api.zingle.me/v1/services/31444380-7c5f-11e7-a433-0f09c13b1321/messages?communication_direction=inbound&page=2&page_size=1000
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'texts_report/dashboard'
+  root 'texts_report#dashboard'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
